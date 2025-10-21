d25c57acdd99c6c6876106c0ad2e3d3991a8e7f6
Lieberr <Gustavoliebfigueira@gmail.com>
2025-10-21 16:31:05 -0300

    Tela Cadastro Pessoa Fisica

--- projeto-integrador/src/App.jsx
+++ projeto-integrador/src/App.jsx
@@ -1,8 +1,10 @@
 import Dashboard_Principal from "./Pages/Dashboard_Principal";
+import CadastroPessoaFisica from "./Pages/CadastroPessoaFisica";
 
 const App = () => {
     return(
-        <Dashboard_Principal />
+        //<Dashboard_Principal />
+        <CadastroPessoaFisica />
     )
 }
 
